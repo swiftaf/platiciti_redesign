724349722ab1567a7195c94f8b37a72547ff4f30
z3301 <scoopnisker@gmail.com>
2023-05-21 16:42:22 -0400

    Initial Commit

---
+++ platiciti_redesign/platiciti_redesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  platiciti_redesignApp.swift
+//  platiciti_redesign
+//
+//  Created by Daniel Zimmerman on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct platiciti_redesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
